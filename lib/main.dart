45047f4e95f8879363d4c5667011f3a466a819dd
SingupalliKartik <skartik1706@gmail.com>
2023-05-20 01:21:03 +0530

    1.1

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,85 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.tealAccent,
+        backgroundColor: Colors.deepPurple[300],
+
         body: SafeArea(
           child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
                 backgroundImage:AssetImage('images/photo.jpg'),
               ),
-              Text("Singupalli kartik",
-              )
+              Text ("Singupalli Kartik",
+                style: GoogleFonts.pacifico(
+                  color: Colors.white,
+fontSize: 35
+                ),),
+                Text("Developer",
+                style: GoogleFonts.poppins(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+                child: Divider(
+                  color:Colors.deepPurple[100] ,
+                )
+              ),
+
+              Card(
+
+                color:Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10,horizontal: 25),
+                child: Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Row(
+                  children: <Widget>[
+                    Icon(Icons.phone,
+                    size: 30,
+                    color: Colors.deepPurple[300],),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('9479782449',
+                   style: GoogleFonts.poppins(
+                     color: Colors.deepPurple[300],
+
+                   ),)
+
+
+                  ],
+              ),
+                ),),
+              Card(
+
+                color:Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10,horizontal: 25),
+                child: Padding(
+                  padding:  EdgeInsets.all(12),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.email,
+                        size: 30,
+                        color: Colors.deepPurple[300],),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text('skartik1706@gmai.com',
+                        style: GoogleFonts.poppins(
+                          color: Colors.deepPurple[300],
+
+
+                        ),)
+
+
+                    ],
+                  ),
+                ),)
 
 
 
